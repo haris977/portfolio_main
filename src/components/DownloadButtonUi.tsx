6c885facae7864cde648fe83cb2c9d4831f79b33
haris977 <harismanzar@gmail.com>
2025-07-07 16:30:32 +0530

    my last commit

--- src/components/DownloadButtonUi.tsx
+++ src/components/DownloadButtonUi.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { HoverBorderGradient } from './ui/BorderGradient'
+
 const DownloadButtonUi = () => {
   return (
     <div>
-      <HoverBorderGradient>
-              <div  className='hover:cursor-pointer'>
-                DOWNLOAD CV
-              </div>
-            </HoverBorderGradient>
+      <a
+        href="https://drive.google.com/uc?export=download&id=1AVihlPqWgR-qZoDApBYo7rbl4otY_2CW
+"        
+        target="_blank"
+        rel="noopener noreferrer"
+        download
+      >
+        <HoverBorderGradient>
+          <div className='hover:cursor-pointer'>
+            DOWNLOAD CV
+          </div>
+        </HoverBorderGradient>
+      </a>
     </div>
   )
 }
