265bc12554038cb03312890c62d2f87b3f6abd27
haris977 <harismanzar@gmail.com>
2025-09-28 10:00:21 +0530

    this is my resume link update main

--- src/components/DownloadButtonUi.tsx
+++ src/components/DownloadButtonUi.tsx
@@ -5,7 +5,7 @@ const DownloadButtonUi = () => {
   return (
     <div>
       <a
-        href="https://drive.google.com/uc?export=download&id=12sjhvluUZY4Nhfm9VXZxGa0K3DDb9qAL"        
+        href="https://drive.google.com/uc?export=download&id=1QWQ7ldKK9Uu9czciF6VgMfZDuF-AmNzk"        
         target="_blank"
         rel="noopener noreferrer"
         download
